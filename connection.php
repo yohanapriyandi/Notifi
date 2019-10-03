145aae20174f77e7a1c4bbd3b062b788b4bdeaec
Yohan89 <cepoangkuningan@gmail.com>
2019-10-04 06:47:43 +0700

    first commit

---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+// change dbname, dbuser, dbpassword(if you use password on mysql if no be blank it)
+$connect = new PDO("mysql:host=localhost;dbname=notif", "root", "");
+session_start();
+?>
---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+    // Credentials for database access
+    // you can use mysqli() or other on example i'am using PDO
+    define('DBINFO', 'mysql:host=localhost;dbname=notificationsystem');
+    define('DBUSER','root');
+    define('DBPASS','');
+    function fetchAll($query){
+        $con = new PDO(DBINFO, DBUSER, DBPASS);
+        $stmt = $con->query($query);
+        return $stmt->fetchAll();
+    }
+    function performQuery($query){
+        $con = new PDO(DBINFO, DBUSER, DBPASS);
+        $stmt = $con->prepare($query);
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+?>
